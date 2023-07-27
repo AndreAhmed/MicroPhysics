761264e36d486575c53a67e1aedd3e44ef95637f
Mahmoud Abdelraouf Mahmoud <mahmoud126125@gmail.com>
2023-07-27 19:53:14 +0300

    Update the main.c -> arrange the source file sections

--- PCPhysicsEngine/main.cpp
+++ PCPhysicsEngine/main.cpp
@@ -1,17 +1,13 @@
-#include <SDL.h>
+/**********************************< Linking Section **********************************/
+#include "../libs/sdl/include/SDL.h"
 #include <stdio.h>
 #include <string>
 #include <cmath>
 
+/**********************************< Global Definition Section **********************************/
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
-
-//Starts up SDL and creates window
-bool init();
-
-//Frees media and shuts down SDL
-void close();
 
 //The window we'll be rendering to
 SDL_Window* gWindow = NULL;
@@ -19,6 +15,65 @@ SDL_Window* gWindow = NULL;
 //The window renderer
 SDL_Renderer* gRenderer = NULL;
 
+/**********************************< Global Declaration Section **********************************/
+//Starts up SDL and creates window
+bool init();
+
+//Frees media and shuts down SDL
+void close();
+
+/**********************************< Main Section **********************************/
+int main(int argc, char* args[])
+{
+	//Start up SDL and create window
+	if (!init())
+	{
+		printf("Failed to initialize!\n");
+	}
+	else
+	{
+		//Main loop flag
+		bool quit = false;
+
+		//Event handler
+		SDL_Event e;
+
+		//While application is running
+		while (!quit)
+		{
+			//Handle events on queue
+			while (SDL_PollEvent(&e) != 0)
+			{
+				//User requests quit
+				if (e.type == SDL_QUIT)
+				{
+					quit = true;
+				}
+			}
+
+			//Clear screen
+			SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+			SDL_RenderClear(gRenderer);
+
+			//Render red filled quad
+			SDL_Rect fillRect = { SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
+			SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
+			SDL_RenderFillRect(gRenderer, &fillRect);
+
+
+			//Update screen
+			SDL_RenderPresent(gRenderer);
+		}
+	}
+
+
+	//Free resources and close SDL
+	close();
+
+	return 0;
+}
+
+/**********************************< Sub Program Section **********************************/
 bool init()
 {
 	//Initialization flag
@@ -75,54 +130,4 @@ void close()
 	gRenderer = NULL;
 
 	SDL_Quit();
-}
-
-int main(int argc, char* args[])
-{
-	//Start up SDL and create window
-	if (!init())
-	{
-		printf("Failed to initialize!\n");
-	}
-	else
-	{
-		//Main loop flag
-		bool quit = false;
-
-		//Event handler
-		SDL_Event e;
-
-		//While application is running
-		while (!quit)
-		{
-			//Handle events on queue
-			while (SDL_PollEvent(&e) != 0)
-			{
-				//User requests quit
-				if (e.type == SDL_QUIT)
-				{
-					quit = true;
-				}
-			}
-
-			//Clear screen
-			SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
-			SDL_RenderClear(gRenderer);
-
-			//Render red filled quad
-			SDL_Rect fillRect = { SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
-			SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
-			SDL_RenderFillRect(gRenderer, &fillRect);
-
-
-			//Update screen
-			SDL_RenderPresent(gRenderer);
-		}
-	}
-
-
-	//Free resources and close SDL
-	close();
-
-	return 0;
 }
